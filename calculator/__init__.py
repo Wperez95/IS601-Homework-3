634001d4e5ca56bc2dc66aa4de9a47e45eb65d2f
whitneyperez <perez.whitney@gmail.com>
2024-02-12 21:44:41 -0500

    added static methods

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,5 +1,23 @@
-def add(a,b):
-    return a + b
+from calculator.calculations import Calculations
+from calculator.operations import add, subtract, multiply, divide
 
-def subtract(a,b):
-    return a - b
+class calculator:
+    @staticmethod
+    def add(a,b):
+        calculation = Calculations(a,b,add) # This function is coming from thr operations folder
+        return calculation.get_result()
+    @staticmethod
+    def subtract (a,b):
+        calculation = Calculations(a,b,subtract)
+        return calculation.get_result()
+    
+    @staticmethod
+    def multiply(a,b):
+        calculation = Calculations(a,b,multiply)
+        return calculation.get_result()
+    
+    @staticmethod
+    def divide(a,b):
+        calculation = Calculations(a,b,divide)
+        return calculation.get_result()
+    
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,10 +1,18 @@
 '''My Calculator Test'''
-from calculator import add, subtract
+from calculator import calculator #importing calculator folders and functions
 
 def test_addition():
-    '''Test that addition function works '''    
-    assert add(2,2) == 4
+    '''Test that addition function works'''    
+    assert calculator.add(2,2) == 4
 
 def test_subtraction():
-    '''Test that addition function works '''    
-    assert subtract(2,2) == 0
+    '''Test that addition function works'''    
+    assert calculator.subtract(2,2) == 0
+
+def test_multiplication():
+    '''Test that multiplication function works'''
+    assert calculator.multiply(2,2) == 4
+
+def test_division():
+    '''Test that division function works'''
+    assert calculator.divide(2,2) == 1
