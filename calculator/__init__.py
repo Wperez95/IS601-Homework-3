6c6ca9decdf2a0333d6ced175f11d6c04a142814
whitneyperez <perez.whitney@gmail.com>
2024-02-14 22:05:01 -0500

    saved part 3 calculator over main brain somehow, edited main branch to original

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,36 +1,5 @@
-from calculator.calculations import Calculations #this manages history of calculations
-from calculator.operations import add, subtract, multiply, divide #these are the math functions
-from calculator.calculation import Calculation #this represents a single calculation
-from decimal import Decimal
-from typing import Callable #for type hinting callable objects
+def add(a.b):
+    return a + b
 
-class Calculator:
-    @staticmethod
-    def _perform_operation(a: Decimal, b: Decimal, operation: Callable[[Decimal, Decimal], Decimal]) -> Decimal:
-        """create and perform a calculation, then return the result"""
-        calculation = Calculation.create(a, b, operation)
-        #creates a calculation object using the static create method, passing in operands and operation
-        Calculations.add_calculation(Calculation)
-        #adds the calculation to the history managed by the calculations class
-        return calculation.perform()
-    
-    @staticmethod
-    def add(a: Decimal, b: Decimal) -> Decimal:
-        #will perform addition by using the _perform_operation method with the add function
-        return Calculator._perform_operation(a,b, add)
-    
-    @staticmethod
-    def subtract(a: Decimal, b: Decimal) -> Decimal:
-        #will perform addition by using the _perform_operation method with the subtract function
-        return Calculator._perform_operation(a,b, subtract)
-    
-    @staticmethod
-    def multiply(a: Decimal, b: Decimal) -> Decimal:
-        #will perform addition by using the _perform_operation method with the multiplication function
-        return Calculator._perform_operation(a,b, multiply)
-    
-    @staticmethod
-    def divide(a: Decimal, b: Decimal) -> Decimal:
-        #will perform addition by using the _perform_operation method with the division function
-        return Calculator._perform_operation(a,b, divide)
-    
+def subtract(a,b):
+    return a - b
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,20 +1,11 @@
-'''My Calculator Test'''
-from calculator import Calculator #importing calculator folders and functions
+'''My test calculator'''
+from calculator import add, subtract
 
 def test_addition():
-    '''Test that addition function works'''    
-    assert Calculator.add(2,2) == 4
+    '''test addition function works'''
+    assert add(3,2) == 5
 
 def test_subtraction():
-    '''Test that addition function works'''    
-    assert Calculator.subtract(2,2) == 0
-
-def test_multiplication():
-    '''Test that multiplication function works'''
-    assert Calculator.multiply(2,2) == 4
-
-def test_division():
-    '''Test that division function works'''
-    assert Calculator.divide(2,2) == 1
-    # Calculator was not defined until you created
-    # a class for calculator in the calculator __init__ folder
+    '''Test that subtraction function works'''
+    assert subtract(3,2) == 1
+    
